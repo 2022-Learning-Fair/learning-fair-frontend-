5bb29b0e31545efe54c9d262dc5b23ee9f09ca3d
nimod7890 <nimod1234@g.skku.edu>
2022-11-11 14:21:04 +0900

    fe: 정리정돈

--- src/js/layouts/footer.js
+++ src/js/layouts/footer.js
@@ -1,6 +1,6 @@
 import React from "react"
 import "../../css/layouts/footer.css"
-import Favicon from "./favicon"
+// import Favicon from "./favicon"
 const Footer = () => {
   return (
     <footer>
